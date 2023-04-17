f8f1107e099cf3c68a834d0c96fdf4978ea905d4
yunnooo <jgr4739@mail.ru>
2023-04-17 15:17:40 +0300

    finished 3th step

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+export default () => {
+    console.log('Welcome to the Brain Games!')
+    const name = readlineSync.question('May i have your name? ');
+    console.log(`Hello, ${name}!`);
+
+
+}
