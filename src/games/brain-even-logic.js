2489dd3a18846973edda694196d70901621c2644
yunnooo <jgr4739@mail.ru>
2023-04-28 16:25:21 +0300

    fix brain-even

--- src/games/brain-even-logic.js
+++ src/games/brain-even-logic.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { gameEngine, generateGameRoundWithAnswerStr as generateRound } from '../index.js';
 
-const task = 'Answer "yes" if number even, otherwise answer "no".';
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (num) => {
   if (num % 2 === 0) {
